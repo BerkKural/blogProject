015f73907f299f01930b613f2c734f56e5865432
Berk <kuralberk@hotmail.com>
2023-03-30 19:03:10 +0300

    add comment lines

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+# Anasayfa fonksiyonu
 def index(request):
     posts = Post.objects.filter(isPublish=True).order_by('-created_at')
     if request.method == 'POST':
